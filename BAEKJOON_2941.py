2ab15881a46ebd74954ee1917c70081189ea8bda
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-08 22:57:40 +0900

    Revert "test with test.py"
    This reverts commit 4ff231be44ffd3e4fc89af2a6c318cc152fd8b8d.

--- BAEKJOON_2941.py
+++
@@ -1,5 +0,0 @@
-word = input()
-cro_al = ['c=','c-','dz=','d-','lj','nj','s=','z=']
-for i in cro_al:
-    word = word.replace(i, '@')
-print(len(word))
--- test.py
+++
@@ -1,2 +0,0 @@
-tuple = (5, 1, 0, 100)
-print(tuple.sorted())
