4ff231be44ffd3e4fc89af2a6c318cc152fd8b8d
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-08 22:45:25 +0900

    test with test.py

--- algorithm/python/BAEKJOON_1037.py
+++
@@ -1,5 +0,0 @@
-test_case = int(input())
-
-divisor_list = list(map(int, input().split()))
-divisor_list.sort()
-print(divisor_list[0]*divisor_list[-1])
---
+++ test.py
@@ -0,0 +1,2 @@
+tuple = (5, 1, 0, 100)
+print(tuple.sorted())
